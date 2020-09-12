44dcb529e4b106401cc4395e0f3ae2585feb8256
hadeel10 <61603080+hadeel10@users.noreply.github.com>
2020-09-12 13:32:47 +0300

    project with out layout just wait

---
+++ app/src/main/java/com/example/saneh/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.saneh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.log_in);
+
+
+
+
+   }
+
+
+
+}
