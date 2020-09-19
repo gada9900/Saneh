63632f7a83803e4012acd8623604f1e1f60fcf29
Shammaa-Alsomaikhi <70749867+Shammaa-Alsomaikhi@users.noreply.github.com>
2020-09-19 22:54:36 +0300

    shammaa1

---
+++ app/src/main/java/com/example/saneh/profile.java
@@ -0,0 +1,48 @@
+package com.example.saneh;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class profile  extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth ;
+    private Button  logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile1);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        logout = (Button)findViewById(R.id.LogOut);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(profile.this,Login.class));
+            }
+        });
+
+
+    }
+}
