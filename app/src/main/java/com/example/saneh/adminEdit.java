dcaa6185f5bb32affefa12d2d6678346253bb30e
Ghada <g.a.g9900@gmail.com>
2020-09-29 01:30:29 +0300

    add admin edit class and layout

--- app/src/main/java/com/example/saneh/adminEdit.java
+++ app/src/main/java/com/example/saneh/adminEdit.java
@@ -1,4 +1,31 @@
 package com.example.saneh;
 
-public class adminEdit {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+public class adminEdit extends AppCompatActivity {
+
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_edit);
+    }
 }
