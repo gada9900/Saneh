70150fcdfa792dcdf56c616bed4d115a3bd3221a
hadeel10 <61603080+hadeel10@users.noreply.github.com>
2020-09-30 19:12:17 +0300

    add code to prevent bottom bar from moving in search lauoyt + manifest returnd to log in page

--- app/src/main/java/com/example/saneh/search.java
+++ app/src/main/java/com/example/saneh/search.java
@@ -10,6 +10,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -44,7 +45,9 @@ public class search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
-
+        //<< to prevent bottom toolbar from moving (its important)
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+       // >>>
         fAuth = FirebaseAuth.getInstance();
 
         inputDate = findViewById(R.id.dateSearch);
