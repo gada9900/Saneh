a4b8df209f3977520becb1d205466a060d05c470
Ghada <g.a.g9900@gmail.com>
2020-10-01 13:07:38 +0300

    navigate between admin's pages

--- app/src/main/java/com/example/saneh/admin_settings.java
+++ app/src/main/java/com/example/saneh/admin_settings.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class admin_settings extends AppCompatActivity {
     private FirebaseAuth firebaseAuth ;
     private Button  logout;
+    private ImageView edit, settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,25 @@ public class admin_settings extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         logout = (Button)findViewById(R.id.LogOut);
+        edit = findViewById(R.id.imageView32);
+        settings = findViewById(R.id.imageView27);
+
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),adminEdit.class);
+                startActivity(i);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),admin_settings.class);
+                startActivity(i);
+            }
+        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
