9f2a8a79726a72ee20a2c798e1674dfbe0e91a25
Ghada <g.a.g9900@gmail.com>
2020-10-03 18:30:26 +0300

    editing adminAdd

--- app/src/main/java/com/example/saneh/adminAdd.java
+++ app/src/main/java/com/example/saneh/adminAdd.java
@@ -2,6 +2,7 @@ package com.example.saneh;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -23,7 +24,11 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class adminAdd extends AppCompatActivity {
@@ -67,13 +72,53 @@ public class adminAdd extends AppCompatActivity {
         classIDPassed = valueFromFirstActivity.getString("classID");
         addClassID = findViewById(R.id.addClassID);
         addClassID.setText(classIDPassed);
-        addClass(classIDPassed);
+
+
+
+        Th8_9 = findViewById(R.id.Th8_9A);
+        Th9_10 = findViewById(R.id.Th9_10A);
+        Th10_11 = findViewById(R.id.Th10_11A);
+        Th11_12 = findViewById(R.id.Th11_12A);
+        Th12_1 = findViewById(R.id.Th12_1A);
+        Th1_2 = findViewById(R.id.Th1_2A);
+        Th2_3 = findViewById(R.id.Th2_3A);
+        S8_9 = findViewById(R.id.S8_9A);
+        S9_10 = findViewById(R.id.S9_10A);
+        S10_11 = findViewById(R.id.S10_11A);
+        S11_12 = findViewById(R.id.S11_12A);
+        S12_1 = findViewById(R.id.S12_1A);
+        S1_2 = findViewById(R.id.S1_2A);
+        S2_3 = findViewById(R.id.S2_3A);
+        W8_9 = findViewById(R.id.W8_9A);
+        W9_10 = findViewById(R.id.W9_10A);
+        W10_11 = findViewById(R.id.W10_11A);
+        W11_12 = findViewById(R.id.W11_12A);
+        W12_1 = findViewById(R.id.W12_1A);
+        W1_2 = findViewById(R.id.W1_2A);
+        W2_3 = findViewById(R.id.W2_3A);
+        M8_9 = findViewById(R.id.M8_9A);
+        M9_10 = findViewById(R.id.M9_10A);
+        M10_11 = findViewById(R.id.M10_11A);
+        M11_12 = findViewById(R.id.M11_12A);
+        M12_1 = findViewById(R.id.M12_1A);
+        M1_2 = findViewById(R.id.M1_2A);
+        M2_3 = findViewById(R.id.M2_3A);
+        T8_9 = findViewById(R.id.T8_9A);
+        T9_10 = findViewById(R.id.T9_10A);
+        T10_11 = findViewById(R.id.T10_11A);
+        T11_12 = findViewById(R.id.T11_12A);
+        T12_1 = findViewById(R.id.T12_1A);
+        T1_2 = findViewById(R.id.T1_2A);
+        T2_3 = findViewById(R.id.T2_3A);
+
     }
 
 
 
-    public void addClass(final String classIDPassed){
+    public boolean addClass(final String classIDPassed){
 
+        final boolean[] Added = new boolean[1];
+        Added[0] = false;
         AddClass = findViewById(R.id.AddClass);
         AddClass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,8 +128,6 @@ public class adminAdd extends AppCompatActivity {
                 @SuppressLint("UseSwitchCompatOrMaterialCode") Switch projector = findViewById(R.id.Projector_switch_add);
                 @SuppressLint("UseSwitchCompatOrMaterialCode") Switch interactive = findViewById(R.id.interactive_switch_add);
 
-                //TextView addClassID = findViewById(R.id.addClassID);
-                //String classID = addClassID.toString().trim();
 
                 if (TextUtils.isEmpty(Capacity.getText().toString().trim())){
                     Capacity.setError("Capacity is required!");
@@ -102,7 +145,10 @@ public class adminAdd extends AppCompatActivity {
                 else
                     newInter = false;
 
+
+
                 //adding classes
+
 
                 DocumentReference documentReference = firebaseFirestore.collection("classes").document(classIDPassed);
 
@@ -111,15 +157,20 @@ public class adminAdd extends AppCompatActivity {
                 newClass.put("capacity", newCap);
                 newClass.put("projector", newPro);
                 newClass.put("interactive", newInter);
-
-                //classes newClass = new classes(classID, newCap, newInter, newPro);
+                newClass.put("s", Arrays.asList(S8_9.isChecked(), S9_10.isChecked(), S10_11.isChecked(), S11_12.isChecked(), S12_1.isChecked(), S1_2.isChecked(), S2_3.isChecked()));
+                newClass.put("m", Arrays.asList(M8_9.isChecked(), M9_10.isChecked(), M10_11.isChecked(), M11_12.isChecked(), M12_1.isChecked(), M1_2.isChecked(), M2_3.isChecked()));
+                newClass.put("t", Arrays.asList(T8_9.isChecked(),T9_10.isChecked(),T10_11.isChecked(),T11_12.isChecked(),T12_1.isChecked(),T1_2.isChecked(),T2_3.isChecked()));
+                newClass.put("w", Arrays.asList(W8_9.isChecked(),W9_10.isChecked(),W10_11.isChecked(),W11_12.isChecked(),W12_1.isChecked(),W1_2.isChecked(),W2_3.isChecked()));
+                newClass.put("th", Arrays.asList(Th8_9.isChecked(),Th9_10.isChecked(),Th10_11.isChecked(),Th11_12.isChecked(),Th12_1.isChecked(),Th1_2.isChecked(),Th2_3.isChecked()));
 
 
                 documentReference.set(newClass)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
-                                Toast.makeText(adminAdd.this, "class added sucessfully", Toast.LENGTH_LONG).show();
+
+                                Toast.makeText(adminAdd.this, "class added successfully", Toast.LENGTH_LONG).show();
+                                Added[0] = true;
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
@@ -127,6 +178,7 @@ public class adminAdd extends AppCompatActivity {
                             public void onFailure(@NonNull Exception e) {
                                 Toast.makeText(adminAdd.this, "Error!", Toast.LENGTH_SHORT).show();
                                 Log.d(TAG, e.toString());
+                                Added[0] = false;
                             }
                         });
             }
@@ -134,8 +186,9 @@ public class adminAdd extends AppCompatActivity {
 
 
 
-
+        return  Added[0];
 
 
     }
 }
+
