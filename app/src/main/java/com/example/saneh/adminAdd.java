c486b5d3fe4e19dc152a1cd146e7bb63fdcdda01
Ghada <g.a.g9900@gmail.com>
2020-10-11 02:51:41 +0300

    minimum capacity is 1, admin condition

--- app/src/main/java/com/example/saneh/adminAdd.java
+++ app/src/main/java/com/example/saneh/adminAdd.java
@@ -145,6 +145,11 @@ public class adminAdd extends AppCompatActivity {
                     return;
                 }
 
+                if (capCheck2 < 1 ) {
+                    Capacity.setError("minimum capacity is 1!");
+                    return;
+                }
+
                 newCap = Long.parseLong(Capacity.getText().toString());
                 if(projector.isChecked())
                     newPro = true;
